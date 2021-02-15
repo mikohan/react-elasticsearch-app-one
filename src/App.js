223c0288deabecc572d97a2cea2392c45fbd2cc5
mikohan <angara99@gmail.com>
2021-02-15 10:20:03 +0300

    Testing react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Container } from '@material-ui/core';
+import ElasticOne from './componenets/ElasticOne.jsx';
 import './App.css';
 
 function App() {
   return (
     <Container maxWidth="lg">
       <h1>Main page</h1>
+      <ElasticOne />
     </Container>
   );
 }
--- src/componenets/ElasticOne.jsx
+++ src/componenets/ElasticOne.jsx
@@ -1,12 +1,14 @@
 import { Elasticsearch, Results } from 'react-elasticsearch';
 import { url } from '../config';
 
-export default function MyComponent() {
+export default function ElasticOne() {
   return (
     <Elasticsearch url={url}>
       <Results
         id="result"
-        items={(data) => data.map((item) => <>{item._source.title}</>)}
+        items={(data) =>
+          data.map((item) => <>{JSON.stringify(item._source)}</>)
+        }
       />
     </Elasticsearch>
   );
